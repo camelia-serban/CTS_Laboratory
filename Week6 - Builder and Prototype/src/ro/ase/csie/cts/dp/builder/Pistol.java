1dbae9f277a1809cea778a225a965ce9b1290786
camelia-serban <72201278+camelia-serban@users.noreply.github.com>
2021-03-30 14:40:02 +0300

    Builder implementation

---
+++ Week6 - Builder and Prototype/src/ro/ase/csie/cts/dp/builder/Pistol.java
@@ -0,0 +1,12 @@
+package ro.ase.csie.cts.dp.builder;
+
+public class Pistol implements WeaponInterface{
+
+	@Override
+	public void pewPew() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Pistol -> -> -> ->");
+	}
+
+}
